5a7af6ca3d45f6a6cd326e04f9d9be173eacd548
alexandru-andronache <al3x.andronache@gmail.com>
2021-08-18 22:48:37 +0200

    problem 17

--- problems/problem_017/main.cpp
+++ problems/problem_017/main.cpp
@@ -11,31 +11,19 @@ namespace problem_017 {
         std::string thousand = "thousand";
         std::string andLiteral = "and";
 
-        for (int i = 1; i <= std::min(n, 9); ++i) {
-            sum += numbers[i].size();
-        }
-        for (int i = 10; i <= std::min(n, 19); ++i) {
-            sum += numbersTeen[i - 10].size();
-        }
-        for (int i = 20; i <= std::min(n, 99); i += 10) {
-            sum += teens[(i - 20) / 10].size();
-            for (int j = i + 1; j <= std::min(n, i + 9); ++j) {
-                sum += teens[(i - 20) / 10].size() + numbers[j - i].size();
-            }
-        }
-
-        for (int k = 100; k <= std::min(n, 999); k += 100) {
-            sum += numbers[k / 100].size() + hundred.size();
+        for (int k = 0; k <= std::min(n, 999); k += 100) {
+            int factor = (k > 0);
+            sum += numbers[k / 100].size() + hundred.size() * factor;
             for (int i = k + 1; i <= std::min(n, k + 9); ++i) {
-                sum += numbers[k / 100].size() + hundred.size() + andLiteral.size() + numbers[i - k].size();
+                sum += numbers[k / 100].size() + (hundred.size() + andLiteral.size()) * factor + numbers[i - k].size();
             }
             for (int i = k + 10; i <= std::min(n, k + 19); ++i) {
-                sum += numbers[k / 100].size() + hundred.size() + andLiteral.size() + numbersTeen[i - k - 10].size();
+                sum += numbers[k / 100].size() + (hundred.size() + andLiteral.size()) * factor + numbersTeen[i - k - 10].size();
             }
             for (int i = k + 20; i <= std::min(n, k + 99); i += 10) {
-                sum += numbers[k / 100].size() + hundred.size() + andLiteral.size() + teens[(i - k - 20) / 10].size();
+                sum += numbers[k / 100].size() + (hundred.size() + andLiteral.size()) * factor + teens[(i - k - 20) / 10].size();
                 for (int j = 1 + i; j <= std::min(n, i + 9); ++j) {
-                    sum += numbers[k / 100].size() + hundred.size() + andLiteral.size() + teens[(i - k - 20) / 10].size() + numbers[j - i].size();
+                    sum += numbers[k / 100].size() + (hundred.size() + andLiteral.size()) * factor + teens[(i - k - 20) / 10].size() + numbers[j - i].size();
                 }
             }
         }
