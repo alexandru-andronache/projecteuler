a69620caea120725bc8f425b4585705b2e32e466
alexandru-andronache <al3x.andronache@gmail.com>
2021-08-24 07:55:50 +0200

    problem 31

--- problems/problem_031/main.cpp
+++ problems/problem_031/main.cpp
@@ -5,24 +5,21 @@
 namespace problem_031 {
     std::vector<int> coins{1, 2, 5, 10, 20, 50, 100, 200};
 
-    void calculate(int sum, int nrCoins, int& sol, int startValue) {
+    int calculate(int sum, int nrCoins, int startValue) {
         if (nrCoins == 0) {
-            sol++;
-            return;
+            return 1;
         }
+        int total = 0;
         for (int i = startValue; i <= sum / coins[nrCoins]; ++i) {
-            calculate(sum - i * coins[nrCoins], nrCoins - 1, sol, 0);
+            total += calculate(sum - i * coins[nrCoins], nrCoins - 1, 0);
         }
+        return total;
     }
 
     int solve(int n) {
-
         std::vector<int> combinations;
-
         for (int i = 0; i < coins.size(); ++i) {
-            int sol = 0;
-            calculate(n, i, sol, 1);
-            combinations.push_back(sol);
+            combinations.push_back(calculate(n, i, 1));
         }
 
         return std::accumulate(combinations.begin(), combinations.end(), 0);
@@ -31,7 +28,7 @@ namespace problem_031 {
 
 #ifndef TESTING
 int main() {
-    std::cout << "Solution : " << problem_031::solve(0) << std::endl;
+    std::cout << "Solution : " << problem_031::solve(200) << std::endl;
 
     return 0;
 }
