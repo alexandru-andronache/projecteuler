4ee9f71c104c2b595c350b23ae99314a7501e6c4
alexandru-andronache <al3x.andronache@gmail.com>
2021-11-09 12:34:30 +0100

    optimize problem 60

--- problems/problem_060/main.cpp
+++ problems/problem_060/main.cpp
@@ -16,14 +16,6 @@ namespace problem_060 {
     }
 
     bool isSol(const std::vector<int>& queue, const std::vector<int>& primes) {
-        if (queue.size() <= 1) {
-            return true;
-        }
-        for (int i = 0; i < queue.size() - 1; ++i) {
-            if (queue[i] == queue.back()) {
-                return false;
-            }
-        }
         for (int i = 0; i < queue.size() - 1; ++i) {
             if (!sieve.isPrime(combine(primes[queue[i]], primes[queue.back()])) ||
                 !sieve.isPrime(combine(primes[queue.back()], primes[queue[i]]))) {
@@ -37,25 +29,26 @@ namespace problem_060 {
         int solMinim = std::numeric_limits<int>::max();
 
         std::vector<int> queue;
-        int sums = 0;
+        int sum = 0;
         queue.push_back(-1);
         while (!queue.empty()) {
             if (queue.back() >= 0) {
-                sums -= primes[queue.back()];
+                sum -= primes[queue.back()];
             }
             queue.back()++;
-            sums += primes[queue.back()];
-            if (sums < solMinim && queue.back() < primes.size() && isSol(queue, primes)) {
+            sum += primes[queue.back()];
+            if (sum < solMinim && queue.back() < primes.size() && isSol(queue, primes)) {
                 if (queue.size() == n) {
-                    solMinim = std::min(solMinim, sums);
+                    solMinim = std::min(solMinim, sum);
                 }
                 else {
-                    queue.push_back(-1);
+                    queue.push_back(queue.back());
+                    sum += primes[queue.back()];
                 }
             }
             else {
                 while (!queue.empty() && queue.back() >= primes.size()) {
-                    sums -= primes[queue.back()];
+                    sum -= primes[queue.back()];
                     queue.pop_back();
                 }
             }
@@ -69,12 +62,11 @@ namespace problem_060 {
         math::SieveEratosthenes se(N);
         std::vector<int> primes;
 
-        for (int i = 2; i < N; ++i) {
+        for (int i = 3; i < N; ++i) {
             if (se.isPrime(i)) {
                 primes.push_back(i);
             }
         }
-
 
         return backtracking(primes, n);
     }
@@ -82,7 +74,7 @@ namespace problem_060 {
 
 #ifndef TESTING
 int main() {
-    std::cout << "Solution : " << problem_060::solve(0) << std::endl;
+    std::cout << "Solution : " << problem_060::solve(5) << std::endl;
 
     return 0;
 }
