94a228af195a49f5a4ed89c2b6c9fec3d7d3de08
alexandru-andronache <al3x.andronache@gmail.com>
2021-11-28 09:40:45 +0100

    add extra cmake targets

--- common/cmake/add_problem.cmake
+++ common/cmake/add_problem.cmake
@@ -1,3 +1,5 @@
+include(${COMMON_SOURCES}/cmake/utilities.cmake)
+
 function(add_problem NUMBER)
     set(NAME problem.${NUMBER})
     set(NAME_TEST ${NAME}.test)
@@ -19,6 +21,32 @@ function(add_problem NUMBER)
     endif()
 
     target_link_libraries(${NAME_TEST}
+            general ${source_dir}/../googletest_utilities-build/googletest/${CMAKE_BUILD_TYPE}Libs/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_DEBUG_POSTFIX}.a
+            general ${source_dir}/../googletest_utilities-build/googletest/${CMAKE_BUILD_TYPE}Libs/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${CMAKE_DEBUG_POSTFIX}.a)
+endfunction()
+
+function(add_set_of_problem START END NAME)
+    set (SOURCES test.cpp ${utils_src})
+    foreach(n RANGE ${START} ${END})
+        pad_number_with_0(${n} 3 n)
+        list(APPEND SOURCES ${CMAKE_CURRENT_SOURCE_DIR}/problems/problem_${n}/main.cpp 
+                            ${CMAKE_CURRENT_SOURCE_DIR}/problems/problem_${n}/test.h
+                            ${CMAKE_CURRENT_SOURCE_DIR}/problems/problem_${n}/tests/TestClass.cpp 
+                            ${CMAKE_CURRENT_SOURCE_DIR}/problems/problem_${n}/tests/TestClass.h)
+    endforeach()
+
+    add_executable(${NAME} ${SOURCES})
+
+    target_include_directories(${NAME} PRIVATE ${COMMON_SOURCES}/include)
+
+    add_dependencies(${NAME} googletest_utilities)
+    target_compile_definitions(${NAME} PRIVATE TESTING=1)
+
+    if (${CMAKE_BUILD_TYPE} STREQUAL "Debug")
+        set(CMAKE_DEBUG_POSTFIX d)
+    endif()
+
+    target_link_libraries(${NAME}
             general ${source_dir}/../googletest_utilities-build/googletest/${CMAKE_BUILD_TYPE}Libs/${CMAKE_FIND_LIBRARY_PREFIXES}gtest${CMAKE_DEBUG_POSTFIX}.a
             general ${source_dir}/../googletest_utilities-build/googletest/${CMAKE_BUILD_TYPE}Libs/${CMAKE_FIND_LIBRARY_PREFIXES}gtest_main${CMAKE_DEBUG_POSTFIX}.a)
 endfunction()
